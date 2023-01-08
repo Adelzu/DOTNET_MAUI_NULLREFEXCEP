f7f311d79cbcfc9b82663d9c074aafc81178748c
Adel Alzubeir <adel@leira.io>
2023-01-08 11:13:13 +0300

    init

---
+++ MAUIBUG/MAUIBUG/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUIBUG;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MAUIBUG/MAUIBUG/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUIBUG;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
